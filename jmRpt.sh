2a6b33f0c0b71c65efb640eadfae011415cb82a7
Godinez, Carlos [ITSUS] <cgodine1@ITS.JNJ.com>
2020-10-06 19:09:43 -0400

    Set default Granularity to 10 secs

--- jmRpt.sh
+++ jmRpt.sh
@@ -2,8 +2,7 @@
 # Carlos A. Godinez
 set -e
 
-#TODO: -Jjmeter.reportgenerator.overall_granularity=1000 -Jjmeter.reportgenerator.report_title="My Report"
-
+GRANULARITY=10000
 PID=$$
 
 if [[ "$#" -eq 1 && -f "$1" && "$1" =~ \.jtl$ ]]; then
@@ -12,26 +11,31 @@ if [[ "$#" -eq 1 && -f "$1" && "$1" =~ \.jtl$ ]]; then
     jtl_file="$1"
 
     if [[ -x "$path_to_jmeter" ]]; then
-		# create report directory 
-		dashboard_dir=$(/usr/local/opt/coreutils/libexec/gnubin/date +"%Y-%m-%d_%T.%3N")
-		mkdir $dashboard_dir
+        # create report directory 
+        dashboard_dir=$(/usr/local/opt/coreutils/libexec/gnubin/date +"%Y-%m-%d_%T.%3N")
+        mkdir $dashboard_dir
 
-		# rename jmeter.log if it contains the jmx filename
-		if [[ -f jmeter.log ]]; then
-			jmx_file=$(grep '.jmx' jmeter.log | cut -d ' ' -f 7)
-			[[ "$jmx_file" ]] && mv jmeter.log jmeter.$PID
-		fi	
+        # rename jmeter.log if it contains the jmx filename
+        if [[ -f jmeter.log ]]; then
+            jmx_file=$(grep '.jmx' jmeter.log | cut -d ' ' -f 7)
+            [[ "$jmx_file" ]] && mv jmeter.log jmeter.$PID
+        fi    
 
-		# build dashboard report
-		$path_to_jmeter -g $jtl_file -o $dashboard_dir
+        # build dashboard report
+        $path_to_jmeter \
+             -g $jtl_file \
+             -o $dashboard_dir \
+             -Jjmeter.reportgenerator.overall_granularity=$GRANULARITY
 
-		# move jtl & jmx files to dashboard directory
-		mv $jtl_file $dashboard_dir
-		[[ -f jmeter.$PID ]] && mv jmeter.$PID $dashboard_dir/jmeter.log
+        # move jtl & jmx files to dashboard directory
+        mv $jtl_file $dashboard_dir
+        [[ "$jmx_file" ]] && cp $jmx_file $dashboard_dir
+        [[ -f errors.jtl ]] && mv errors.jtl $dashboard_dir
+        [[ -f jmeter.$PID ]] && mv jmeter.$PID $dashboard_dir/jmeter.log
 
-  	else
-		echo "$0: Jmeter executable not found"
-		exit 1
+      else
+        echo "$0: Jmeter executable not found"
+        exit 1
     fi
 else
     echo "Usage: $0 jmeter-file.jtl"
